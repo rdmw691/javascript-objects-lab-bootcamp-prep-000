16b272c28a823417e05dfc0102168c3bb528887d
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-16 18:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]:value})
 }
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, recipes, {key:[value]})
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object.key = value
-  return object
-}NOPE
+function deleteFromObjectByKey(object, key) {
+  delete recipes[key]
+  return recipes
+}
+
